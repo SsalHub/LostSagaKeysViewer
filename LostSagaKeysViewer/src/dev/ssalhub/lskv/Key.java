6a31f1c0d7fc8ad1dd81d200838b8d5525323a12
sjw000324@gmail.com <sjw000324@gmail.com>
2024-04-03 20:01:30 +0900

    chore : init project

--- LostSagaKeysViewer/src/dev/ssalhub/lskv/Key.java
+++ LostSagaKeysViewer/src/dev/ssalhub/lskv/Key.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package dev.roanh.kps;
+package dev.ssalhub.lskv;
 
 import dev.ssalhub.lskv.config.group.KeyPanelSettings;
 import dev.ssalhub.lskv.panels.KeyPanel;
